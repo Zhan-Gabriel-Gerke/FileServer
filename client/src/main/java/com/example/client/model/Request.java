1458e97bed7d4bfbb589d48c8de2e20a571a615b
Zhan-Gabriel Gerke <zangerke3@gmail.com>
2025-09-27 15:46:56 +0300

    new models for client

---
+++ client/src/main/java/com/example/client/model/Request.java
@@ -0,0 +1,13 @@
+package com.example.client.model;
+
+public class Request {
+
+    private String identifier;
+    private byte[] data;
+    private String type;
+    private String arg;
+
+
+
+
+}
