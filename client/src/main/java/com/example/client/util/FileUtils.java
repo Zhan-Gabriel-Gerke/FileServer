76c4889128ba419bd1afa1798c8b76c3dc996abc
Zhan-Gabriel Gerke <93312041+Zhan-Gabriel-Gerke@users.noreply.github.com>
2025-12-13 18:34:35 +0200

    BugFixing

--- client/src/main/java/com/example/client/util/FileUtils.java
+++ client/src/main/java/com/example/client/util/FileUtils.java
@@ -1,18 +1,33 @@
 package com.example.client.util;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 
 public class FileUtils {
 
-    private static final File fileDir = new File("C:\\Users\\zange\\IdeaProjects\\FileServer\\client\\src\\main\\java\\com\\example\\client\\data");
+    private static final String DATA_FOLDER = "client" + File.separator + "data";
+    private static final File fileDir = new File(DATA_FOLDER);
+
+    static {
+        if (!fileDir.exists()) {
+            boolean created = fileDir.mkdirs();
+            if (created) {
+                System.out.println("Created client data directory: " + fileDir.getAbsolutePath());
+            }
+        }
+    }
 
     public static byte[] getFile(String fileName) {
         try {
             File filePath = new File(fileDir, fileName);
+            if (!filePath.exists()) {
+                System.out.println("File not found: " + fileName);
+                return new byte[0];
+            }
             return Files.readAllBytes(filePath.toPath());
-        } catch (Exception e) {
-            System.out.println("File not found: " + fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
             return new byte[0];
         }
     }
@@ -21,8 +36,10 @@ public class FileUtils {
         try {
             File filePath = new File(fileDir, fileName);
             Files.write(filePath.toPath(), content);
-        } catch (Exception e) {
+            System.out.println("File saved to: " + filePath.getAbsolutePath());
+        } catch (IOException e) {
             System.out.println("Cannot write file: " + fileName);
+            e.printStackTrace();
         }
     }
 }
